30a64915a7dc048e3aaa61b9ef204a5fec688459
thomasasia <thomas.harriett@gmail.com>
2021-11-11 00:34:00 -0500

    added function to get uid

--- app.py
+++ app.py
@@ -149,6 +149,15 @@ def encode_string(id_list):
         ids_str += id
     return ids_str
 
+# returns the UID of the given username.
+# returns -1 if the username doesn't exist in the database.
+get uid_by_username(username):
+    acc = Account.query.filter_by(username=username).first()
+    if acc is None:
+        return -1
+    else:
+        return acc.uid
+
 def get_account_db_entry(uid):
     acc = Account.query.filter_by(uid=uid).first()
     return acc
