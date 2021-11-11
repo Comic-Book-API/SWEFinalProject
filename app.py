ca7ecffcf57ecfdc58159326ee0f0b00cd661de7
thomasasia <thomas.harriett@gmail.com>
2021-11-10 21:59:44 -0500

    removed DC stuff

--- app.py
+++ app.py
@@ -13,16 +13,17 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://tmp.db"
 # Database config and setup
 db = SQLAlchemy(app)
 
+
+# TODO:
+# password cryptography
+# 
 class Account(db.Model):
     uid = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.BigInteger, nullable=False)
     # The following fields encode a list of up to 20 id's into a string.
     # This code assumes that the max length for any one of these ids is 8 digits/characters
-
-    DC_comics = db.Column(db.String(200))
     Marvel_comics = db.Column(db.String(200))
-    DC_characters = db.Column(db.String(200))
     Marvel_characters = db.Column(db.String(200))
 
 db.create_all()
