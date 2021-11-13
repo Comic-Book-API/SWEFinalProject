03477eb1c3dc03ef729543d596efa61508f39d52
PhaiNguyen89 <pnguyen89@student.gsu.edu>
2021-11-13 20:59:00 +0000

    fixes

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import getpass
 from dotenv import find_dotenv, load_dotenv
 from cryptography.fernet import Fernet
 from flask_login import LoginManager
+import marvel_api as marvel
 
 app = flask.Flask(__name__)
 
@@ -18,22 +19,24 @@ login_manager.init_app(app)
 load_dotenv(find_dotenv())
 
 
-
-
-
 CRYPTOGRAPHY_KEY = os.getenv("CRYPTO_KEY").encode("UTF-8")
 encryption_engine = Fernet(CRYPTOGRAPHY_KEY)
 # database location
 
-engine = create_engine("postgresql://hxbfjmfvfojzkv:f307f9122a40563211c43b4bbbf03e8ac9e8e2d3c0525aa3b1b462ee2ca48810@ec2-23-23-133-10.compute-1.amazonaws.com:5432/d9nffekjuq21dl")#("postgresql://localhost/tmp.db")
+engine = create_engine(
+    "postgresql://hxbfjmfvfojzkv:f307f9122a40563211c43b4bbbf03e8ac9e8e2d3c0525aa3b1b462ee2ca48810@ec2-23-23-133-10.compute-1.amazonaws.com:5432/d9nffekjuq21dl"
+)  # ("postgresql://localhost/tmp.db")
 if not database_exists(engine.url):
     create_database(engine.url)
 
-app.config["SQLALCHEMY_DATABASE_URI"] =  engine.url
+app.config["SQLALCHEMY_DATABASE_URI"] = engine.url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # overhead is bad
 # Database config and setup
 db = SQLAlchemy(app)
-MAX_ID_ENTRIES = 20 # If this value is made greater, the database must be completely reinitialized
+MAX_ID_ENTRIES = (
+    20  # If this value is made greater, the database must be completely reinitialized
+)
+
 
 class Account(db.Model):
     uid = db.Column(db.Integer, primary_key=True)
@@ -48,6 +51,7 @@ class Account(db.Model):
 
     # Login stuff
     authenticated = False
+
     def is_active(self):
         return True
 
@@ -70,27 +74,30 @@ def user_loader(username):
     else:
         return None
 
+
 db.create_all()
 db.session.commit()
 
 # returns encrypted hash
 def encrypt(word):
-    word = word.encode('UTF-8')
+    word = word.encode("UTF-8")
     hash = encryption_engine.encrypt(word)
-    hash = hash.decode('UTF-8')
+    hash = hash.decode("UTF-8")
     return hash
 
+
 def decrypt(hash):
-    hash = hash.encode('UTF-8')
+    hash = hash.encode("UTF-8")
     word = encryption_engine.decrypt(hash)
-    word = word.decode('UTF-8')
+    word = word.decode("UTF-8")
     return word
+
 
 # adds a new account to the database.
 # Make sure password is encrypted!!
 # returns 0 if successful, -1 if the username already exists
 def add_account(username, password):
-    new_acc = Account(username = username, password = password, comics="", characters="")
+    new_acc = Account(username=username, password=password, comics="", characters="")
     test_acc = Account.query.filter_by(username=username).first()
     if test_acc is not None:
         if test_acc.username == username:
@@ -98,6 +105,7 @@ def add_account(username, password):
     db.session.add(new_acc)
     db.session.commit()
     return 0
+
 
 # removes comic from the comics entry for the given user
 # returns 0 if successful, -1 if the comic could not be found
@@ -110,6 +118,7 @@ def remove_comic(uid, comic_id):
     db.session.commit()
     return 0
 
+
 # removes character from the characters entry for the given user
 # returns 0 if successful, -1 if the entry could not be found
 def remove_character(uid, character_id):
@@ -120,6 +129,7 @@ def remove_character(uid, character_id):
     get_account_db_entry(uid).characters = encode_string(characters)
     db.session.commit()
     return 0
+
 
 # adds comic from the comics entry for the given user
 # returns 0 if successful, -1 if the entry could not be added because the max has been reached, and -2 if the entry is already present
@@ -134,6 +144,7 @@ def add_comic(uid, comic_id):
     db.session.commit()
     return 0
 
+
 # adds character from the character entry for the given user
 # returns 0 if successful, -1 if the entry could not be added because the max has been reached, and -2 if the entry is already present
 def add_character(uid, character_id):
@@ -147,6 +158,7 @@ def add_character(uid, character_id):
     get_account_db_entry(uid).characters = encode_string(characters)
     db.session.commit()
     return 0
+
 
 # decodes a string, returning the encoded list of ids
 # return value of None means that the encoded string is invalid somehow
@@ -164,15 +176,18 @@ def decode_string(encoded_string):
             return None
     return id_list
 
+
 # returns the character at the given index
 def get_character(uid, character_index):
-    characters = decode_string(get_accound_db_characters(uid))
+    characters = decode_string(get_account_db_characters(uid))
     return characters[character_index]
+
 
 # returns the comci at the given index
 def get_comic(uid, comic_index):
     comics = decode_string(get_account_db_comics(uid))
     return comics[comic_index]
+
 
 def encode_string(id_list):
     ids_str = ""
@@ -192,16 +207,20 @@ def uid_by_username(username):
     else:
         return acc.uid
 
+
 def get_account_db_entry(uid):
     acc = Account.query.filter_by(uid=uid).first()
     return acc
+
+
 def get_account_db_comics(uid):
     return get_account_db_entry(uid).comics
+
+
 def get_account_db_characters(uid):
     return get_account_db_entry(uid).characters
 
 
-<<<<<<< HEAD
 @app.route("/search", methods=["POST", "GET"])
 def search():
     if flask.request.method == "GET":
@@ -222,15 +241,15 @@ def search():
         )
 
 
-=======
->>>>>>> a1c4a79b9f6573032931c0b1b4e25bcb4170dbd3
 @app.route("/")
 def index():
-    return flask.render_template("index.html") #signup.html
+    return flask.render_template("index.html")  # signup.html
+
 
 @app.route("/signup")
 def signup():
     return flask.render_template("signup.html")
+
 
 @app.route("/signup", methods=["POST"])
 def register():
@@ -239,22 +258,25 @@ def register():
 
     # we just need database to continue
 
-    return flask.redirect("/") #change to login.html
+    return flask.redirect("/")  # change to login.html
+
 
 @app.route("/quiz")
 def quiz():
     return flask.render_template("quiz.html")
 
+
 # Example signin logic:
-@app.route('/asdf', methods=['POST','GET'])
+@app.route("/asdf", methods=["POST", "GET"])
 def sign_in():
     if flask.request.method == "POST":
-        username = request.form.get('username')
-        password = encrypt(request.form.get('password'))
+        username = flask.request.form.get("username")
+        password = encrypt(flask.request.form.get("password"))
         user = Account.query.filter_by(username=username)
         if user:
             if password == user.password:
                 user.authenticated = True
-                flask_login.login_user(user)
+                flask.flask_login.login_user(user)
+
 
 app.run()
