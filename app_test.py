d827846aa5dd4445c870b795a916afced64776b4
trider3 <60563694+trider3@users.noreply.github.com>
2021-12-02 23:44:55 -0500

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -1,13 +1,14 @@
+"""this is the testing file"""
+import os
+import unittest
 import flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
-import os
 from dotenv import find_dotenv, load_dotenv
 from cryptography.fernet import Fernet
 from flask_login import LoginManager
 
-import unittest
 from app import remove_comic, remove_character, add_comic, add_character
 
 app = flask.Flask(__name__)
@@ -25,8 +26,11 @@ encryption_engine = Fernet(CRYPTOGRAPHY_KEY)
 # database location
 
 engine = create_engine(
-    "postgresql://hxbfjmfvfojzkv:f307f9122a40563211c43b4bbbf03e8ac9e8e2d3c0525aa3b1b462ee2ca48810@ec2-23-23-133-10.compute-1.amazonaws.com:5432/d9nffekjuq21dl"
+    "postgresql://hxbfjmfvfojzkv:f307f9122a40563211c43b4bbbf03e8ac9e8e2d3c0525aa3b1b4"
+    "62ee2ca48810@ec2-23-23-133-10.compute-1.amazonaws.com:5432/d9nffekjuq21dl"
 )
+
+
 if not database_exists(engine.url):
     create_database(engine.url)
 
@@ -40,6 +44,7 @@ MAX_ID_ENTRIES = (
 
 
 class Account(db.Model):
+    """db table schema"""
     uid = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
@@ -50,15 +55,19 @@ class Account(db.Model):
     authenticated = False
 
     def is_active(self):
+        """check if active"""
         return True
 
     def get_id(self):
+        """get uid"""
         return self.uid
 
     def is_authenticated(self):
+        """get authentication status"""
         return self.authenticated
 
     def is_anonymous(self):
+        """check if user is anonymous"""
         return False
 
 
@@ -69,28 +78,29 @@ print(Account.query.with_entities(Account.characters).filter(Account.uid == 1).o
 
 
 class GetURLTestCase(unittest.TestCase):
-
-    def test_exists(self):
-        assert(db.session.query(Account).all())
-
-#########comic testing##########
+    """all test cases"""
+    #########comic testing##########
     def test_a_add_multi_comic(self):
+        """test case for adding multiple comics"""
         add_comic(1, '1234')
         add_comic(1, '9876')
         user_comics = Account.query.with_entities(Account.comics).filter(Account.uid == 1).one()
         self.assertEqual(user_comics[0], '1234;9876', 'Multi Add Comic Failed')
 
     def test_b_add_dupe_comic(self):
+        """test case for adding duplicate comic"""
         add_comic(1, '1234')
         user_comics = Account.query.with_entities(Account.comics).filter(Account.uid == 1).one()
         self.assertEqual(user_comics[0], '1234;9876', 'Duplicate Add Comic Failed')
 
     def test_c_remove_comic(self):
+        """test case for deleting single comic"""
         remove_comic(1, '1234')
         user_comics = Account.query.with_entities(Account.comics).filter(Account.uid == 1).one()
         self.assertEqual(user_comics[0], '9876', 'Remove Single Comic Failed')
 
     def test_d_add_del_comic(self):
+        """test case for adding and deleting comics"""
         add_comic(1, '1111')
         remove_comic(1, '9876')
         remove_comic(1, '1111')
@@ -99,27 +109,32 @@ class GetURLTestCase(unittest.TestCase):
 
 #########character testing##########
     def test_e_add_multi_character(self):
+        """test case for adding multiple characters"""
         add_character(1, '1234')
         add_character(1, '9876')
         user_chars = Account.query.with_entities(Account.characters).filter(Account.uid == 1).one()
         self.assertEqual(user_chars[0], '1234;9876', 'Multi Add Character Failed')
 
     def test_f_add_dupe_character(self):
+        """test case for adding duplicate character"""
         add_character(1, '1234')
         user_chars = Account.query.with_entities(Account.characters).filter(Account.uid == 1).one()
         self.assertEqual(user_chars[0], '1234;9876', 'Duplicate Add Character Failed')
 
     def test_g_remove_character(self):
+        """test case for remove single character"""
         remove_character(1, '1234')
         user_chars = Account.query.with_entities(Account.characters).filter(Account.uid == 1).one()
         self.assertEqual(user_chars[0], '9876', 'Remove Single Character Failed')
 
     def test_h_add_del_character(self):
+        """test case for adding and deleting characters"""
         add_character(1, '1111')
         remove_character(1, '9876')
         remove_character(1, '1111')
         user_chars = Account.query.with_entities(Account.characters).filter(Account.uid == 1).one()
         self.assertEqual(user_chars[0], '', 'Compound Operation Character Failed')
+
 
 if __name__ == "__main__":
     unittest.main()
